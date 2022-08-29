060d0240dd576929730781da76ca5ecb85e2e3a3
nukeykt <alexeytf2@gmail.com>
2022-08-29 20:14:08 +0900

    z80 wip

--- z80.c
+++ z80.c
@@ -14,4 +14,91 @@ int Z80_GetData(z80_t *chip)
 
 void Z80_Clock(z80_t *chip, int clk)
 {
+    if (clk && chip->w3 && chip->tm_w2)
+        chip->w1 = 1;
+    if (chip->tm_w4 || (clk && (chip->tm_w5 || chip->tm_w6)))
+        chip->w1 = 0;
+
+    if (clk && chip->tm_w1 && chip->tm_w6)
+        chip->w2 = 0;
+    if (clk)
+        chip->l1 = chip->tm_w2;
+    if (!clk && chip->l1)
+        chip->w2 = 0;
+    if (clk && chip->w15)
+        chip->w2 = 1;
+    chip->w3 = !(chip->tm_w5 || chip->tm_w4);
+
+    if (!clk)
+        chip->w4 = chip->i_int;
+    else
+        chip->w5 = chip->w4;
+
+    if (clk)
+        chip->l2 = chip->w17;
+
+    if (!chip->i_nmi)
+        chip->w7 = 1;
+    if (!chip->l2)
+        chip->w7 = 0;
+
+    if (chip->i_nmi)
+        chip->w6 = chip->w7;
+
+    if (!clk)
+        chip->w8 = chip->w6;
+    else
+        chip->w9 = chip->w8;
+
+    if (clk)
+        chip->l3 = chip->tm_w1;
+    chip->w12 = !(chip->w5 || (chip->w9 && chip->l3));
+    chip->w11 = !(chip->w12 || chip->w9 || chip->tm_w3);
+    chip->w10 = !(chip->w12 || chip->w9 || chip->w11);
+    chip->w13 = !((chip->w16 && !chip->w10) || chip->w18 || chip->w19 || chip->halt);
+    chip->w14 = !(chip->w13 || (chip->w16 && chip->w10));
+    chip->w15 = !(chip->tm_w1 || chip->tm_w2 || chip->tm_w3);
+    if (clk)
+        chip->l4 = chip->w20;
+    chip->w16 = chip->l4 && !clk;
+    chip->w17 = !(chip->tm_w1 || chip->tm_w2);
+    if (chip->w16)
+        chip->w18 = chip->w12;
+    if (chip->tm_w1)
+        chip->w18 = 0;
+    if (chip->w16)
+        chip->w19 = chip->w9;
+    if (chip->tm_w1)
+        chip->w19 = 0;
+    if (chip->w13 && chip->w16)
+        chip->halt = 1;
+    if (chip->tm_w1 || chip->tm_w2 || chip->tm_w3 || chip->tm_w4)
+        chip->halt = 0;
+    chip->o_halt = chip->halt;
+    chip->w20 = !(chip->tm_w1 || chip->tm_w2 || !chip->tm_w3 || chip->tm_w4);
+    if (chip->tm_w2 || chip->tm_w3)
+        chip->w21 = 0;
+    if (chip->w23)
+        chip->w21 = 1;
+    chip->o_mreq = state_z;
+    if (!chip->tm_w4 && !chip->w21)
+        chip->o_mreq = state_0;
+    if (chip->w21)
+        chip->o_mreq = state_1;
+    if (chip->tm_w2 || chip->tm_w3)
+        chip->w22 = 0;
+    if (chip->w23 || (chip->tm_w5 && clk))
+        chip->w22 = 1;
+    if (!chip->tm_w1 && !chip->w22)
+        chip->o_iorq = state_0;
+    if (chip->w22)
+        chip->o_iorq = state_1;
+    if (clk)
+        chip->l5 = chip->tm_w1;
+    chip->w23 = !clk && !chip->l5;
+    if (clk)
+        chip->l6 = chip->tm_w2;
+    chip->w24 = !clk && !chip->tm_w1 && !chip->l6;
+
+    chip->w25 = !(chip->w24 || chip->w23 || (chip->tm_w1 && clk));
 }
--- z80.h
+++ z80.h
@@ -1,15 +1,57 @@
 #pragma once
 
+enum {
+    state_0 = 0,
+    state_1,
+    state_z
+};
+
 typedef struct {
     // input
+    int i_int;
+    int i_nmi;
 
     // output
 
     int ext_data_i;
     int ext_data_o;
+    int o_mreq;
+    int o_halt;
+    int o_iorq;
 
     // state
     int w1;
+    int w2;
+    int l1;
+    int w3;
+    int w4;
+    int w5;
+    int w6;
+    int w7;
+    int w8;
+    int w9;
+    int l2;
+    int w10;
+    int w11;
+    int w12;
+    int l3;
+    int halt;
+    int w13;
+    int w14;
+    int w15;
+    int w16;
+    int l4;
+    int w17;
+    int w18;
+    int w19;
+    int w20;
+    int w21;
+    int w22;
+    int w23;
+    int l5;
+    int w24;
+    int l6;
+    int w25;
 
     // temp wires
     int tm_w1;
@@ -17,5 +59,6 @@ typedef struct {
     int tm_w3;
     int tm_w4;
     int tm_w5;
+    int tm_w6;
 
 } z80_t;
