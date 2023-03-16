b64b3362b1f01241e616e896e3921f38d18a886b
nukeykt <alexeytf2@gmail.com>
2023-03-16 13:56:18 +0900

    ym7101 skeleton

---
+++ vdp.c
@@ -0,0 +1,10 @@
+// YM7101 core
+#include "vdp.h"
+
+void VDP_ClockMCLK(vdp_t *chip, int mclk)
+{
+}
+
+void VDP_ClockDCLK(vdp_t *chip, int clk1, int clk2)
+{
+}
---
+++ vdp.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdint.h>
+
+typedef struct {
+    int t;
+} vdp_t;
