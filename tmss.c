cb06a08a4a75ac2b90931d97fdbb2f3a04269d4c
ogamespec <ogamespec@gmail.com>
2024-05-12 09:49:34 +0300

    TMSS_ENABLE macro

--- tmss.c
+++ tmss.c
@@ -50,8 +50,11 @@ void TMSS_Clock(tmss_t *chip)
     chip->w41 = !(chip->input.ext_rw_in && chip->w15);
 
 
+#if TMSS_ENABLE
     SDFFR_Update(&chip->dff3, !chip->w23 || chip->input.ext_rw_in, (chip->input.ext_data_in & 1) != 0, chip->input.ext_sres);
-    //SDFFS_Update(&chip->dff3, !chip->w23 || chip->input.ext_rw_in, (chip->input.ext_data_in & 1) != 0, chip->input.ext_sres);
+#else
+    SDFFS_Update(&chip->dff3, !chip->w23 || chip->input.ext_rw_in, (chip->input.ext_data_in & 1) != 0, chip->input.ext_sres);
+#endif
 
     chip->w31 = chip->input.ext_cart || !chip->input.ext_m3;
     chip->w28 = chip->dff3.l2 || chip->w31 || chip->input.ext_ce0_arb;
